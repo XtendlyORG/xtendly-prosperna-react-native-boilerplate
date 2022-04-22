74839b7e30b77082122193ae41a2814ce7be1b5b
jericoxnavarro <93170504+JericoK@users.noreply.github.com>
2022-04-22 17:11:36 +0800

    📝 (feat:) guides

--- documentation/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/jericoxnavarro/Documents/GitHub/xtendly-prosperna-react-native-boilerplate/documentation/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/jericoxnavarro/Documents/GitHub/xtendly-prosperna-react-native-boilerplate/documentation/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/jericoxnavarro/Documents/GitHub/xtendly-prosperna-react-native-boilerplate/documentation/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
-  require("/Users/jericoxnavarro/Documents/GitHub/xtendly-prosperna-react-native-boilerplate/documentation/src/css/custom.css"),
-];
