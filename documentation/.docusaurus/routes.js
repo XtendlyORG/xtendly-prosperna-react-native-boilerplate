1d40e5d0f9743448160621bb4bcc66058e911548
jericoxnavarro <93170504+JericoK@users.noreply.github.com>
2022-04-22 17:10:44 +0800

    📝 (feat:) guides

---
+++ documentation/.docusaurus/routes.js
@@ -0,0 +1,169 @@
+
+import React from 'react';
+import ComponentCreator from '@docusaurus/ComponentCreator';
+
+export default [
+  {
+    path: '/xtendly-prosperna-react-native-boilerplate/__docusaurus/debug',
+    component: ComponentCreator('/xtendly-prosperna-react-native-boilerplate/__docusaurus/debug','dc4'),
+    exact: true
+  },
+  {
+    path: '/xtendly-prosperna-react-native-boilerplate/__docusaurus/debug/config',
+    component: ComponentCreator('/xtendly-prosperna-react-native-boilerplate/__docusaurus/debug/config','acd'),
+    exact: true
+  },
+  {
+    path: '/xtendly-prosperna-react-native-boilerplate/__docusaurus/debug/content',
+    component: ComponentCreator('/xtendly-prosperna-react-native-boilerplate/__docusaurus/debug/content','528'),
+    exact: true
+  },
+  {
+    path: '/xtendly-prosperna-react-native-boilerplate/__docusaurus/debug/globalData',
+    component: ComponentCreator('/xtendly-prosperna-react-native-boilerplate/__docusaurus/debug/globalData','bc6'),
+    exact: true
+  },
+  {
+    path: '/xtendly-prosperna-react-native-boilerplate/__docusaurus/debug/metadata',
+    component: ComponentCreator('/xtendly-prosperna-react-native-boilerplate/__docusaurus/debug/metadata','71c'),
+    exact: true
+  },
+  {
+    path: '/xtendly-prosperna-react-native-boilerplate/__docusaurus/debug/registry',
+    component: ComponentCreator('/xtendly-prosperna-react-native-boilerplate/__docusaurus/debug/registry','e59'),
+    exact: true
+  },
+  {
+    path: '/xtendly-prosperna-react-native-boilerplate/__docusaurus/debug/routes',
+    component: ComponentCreator('/xtendly-prosperna-react-native-boilerplate/__docusaurus/debug/routes','335'),
+    exact: true
+  },
+  {
+    path: '/xtendly-prosperna-react-native-boilerplate/blog',
+    component: ComponentCreator('/xtendly-prosperna-react-native-boilerplate/blog','505'),
+    exact: true
+  },
+  {
+    path: '/xtendly-prosperna-react-native-boilerplate/blog/2020/04/19/React-Native-Boilerplate-3.0.0',
+    component: ComponentCreator('/xtendly-prosperna-react-native-boilerplate/blog/2020/04/19/React-Native-Boilerplate-3.0.0','a67'),
+    exact: true
+  },
+  {
+    path: '/xtendly-prosperna-react-native-boilerplate/blog/archive',
+    component: ComponentCreator('/xtendly-prosperna-react-native-boilerplate/blog/archive','3ce'),
+    exact: true
+  },
+  {
+    path: '/xtendly-prosperna-react-native-boilerplate/blog/tags',
+    component: ComponentCreator('/xtendly-prosperna-react-native-boilerplate/blog/tags','1de'),
+    exact: true
+  },
+  {
+    path: '/xtendly-prosperna-react-native-boilerplate/blog/tags/boilerplate',
+    component: ComponentCreator('/xtendly-prosperna-react-native-boilerplate/blog/tags/boilerplate','c2a'),
+    exact: true
+  },
+  {
+    path: '/xtendly-prosperna-react-native-boilerplate/blog/tags/javascript',
+    component: ComponentCreator('/xtendly-prosperna-react-native-boilerplate/blog/tags/javascript','b6c'),
+    exact: true
+  },
+  {
+    path: '/xtendly-prosperna-react-native-boilerplate/blog/tags/kit',
+    component: ComponentCreator('/xtendly-prosperna-react-native-boilerplate/blog/tags/kit','9b7'),
+    exact: true
+  },
+  {
+    path: '/xtendly-prosperna-react-native-boilerplate/blog/tags/native',
+    component: ComponentCreator('/xtendly-prosperna-react-native-boilerplate/blog/tags/native','994'),
+    exact: true
+  },
+  {
+    path: '/xtendly-prosperna-react-native-boilerplate/blog/tags/react',
+    component: ComponentCreator('/xtendly-prosperna-react-native-boilerplate/blog/tags/react','46e'),
+    exact: true
+  },
+  {
+    path: '/xtendly-prosperna-react-native-boilerplate/blog/tags/react-native',
+    component: ComponentCreator('/xtendly-prosperna-react-native-boilerplate/blog/tags/react-native','f00'),
+    exact: true
+  },
+  {
+    path: '/xtendly-prosperna-react-native-boilerplate/blog/tags/starter',
+    component: ComponentCreator('/xtendly-prosperna-react-native-boilerplate/blog/tags/starter','41c'),
+    exact: true
+  },
+  {
+    path: '/xtendly-prosperna-react-native-boilerplate/blog/tags/starter-kit',
+    component: ComponentCreator('/xtendly-prosperna-react-native-boilerplate/blog/tags/starter-kit','d3d'),
+    exact: true
+  },
+  {
+    path: '/xtendly-prosperna-react-native-boilerplate/blog/tags/typescript',
+    component: ComponentCreator('/xtendly-prosperna-react-native-boilerplate/blog/tags/typescript','494'),
+    exact: true
+  },
+  {
+    path: '/xtendly-prosperna-react-native-boilerplate/docs',
+    component: ComponentCreator('/xtendly-prosperna-react-native-boilerplate/docs','b9d'),
+    routes: [
+      {
+        path: '/xtendly-prosperna-react-native-boilerplate/docs/BetaBuild',
+        component: ComponentCreator('/xtendly-prosperna-react-native-boilerplate/docs/BetaBuild','f99'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      },
+      {
+        path: '/xtendly-prosperna-react-native-boilerplate/docs/Components',
+        component: ComponentCreator('/xtendly-prosperna-react-native-boilerplate/docs/Components','37f'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      },
+      {
+        path: '/xtendly-prosperna-react-native-boilerplate/docs/Configuration',
+        component: ComponentCreator('/xtendly-prosperna-react-native-boilerplate/docs/Configuration','5a3'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      },
+      {
+        path: '/xtendly-prosperna-react-native-boilerplate/docs/Installation',
+        component: ComponentCreator('/xtendly-prosperna-react-native-boilerplate/docs/Installation','d3a'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      },
+      {
+        path: '/xtendly-prosperna-react-native-boilerplate/docs/Introduction',
+        component: ComponentCreator('/xtendly-prosperna-react-native-boilerplate/docs/Introduction','f10'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      },
+      {
+        path: '/xtendly-prosperna-react-native-boilerplate/docs/Navigation',
+        component: ComponentCreator('/xtendly-prosperna-react-native-boilerplate/docs/Navigation','a3b'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      },
+      {
+        path: '/xtendly-prosperna-react-native-boilerplate/docs/StateManagement',
+        component: ComponentCreator('/xtendly-prosperna-react-native-boilerplate/docs/StateManagement','c1e'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      },
+      {
+        path: '/xtendly-prosperna-react-native-boilerplate/docs/UsingFlipper',
+        component: ComponentCreator('/xtendly-prosperna-react-native-boilerplate/docs/UsingFlipper','0f8'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      }
+    ]
+  },
+  {
+    path: '/xtendly-prosperna-react-native-boilerplate/',
+    component: ComponentCreator('/xtendly-prosperna-react-native-boilerplate/','92d'),
+    exact: true
+  },
+  {
+    path: '*',
+    component: ComponentCreator('*')
+  }
+];
